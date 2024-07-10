56b5b5ad3475800103efb5d7fe22116f7dd1a670
Gollavilli Dhanush Kumar <112778628+Dhanush2468@users.noreply.github.com>
2024-07-10 21:32:42 +0000

    sss

---
+++ android/app/src/main/java/com/anveshna/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.anveshna.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anveshna.app',
+  appName: 'anveshna',
+  webDir: 'build'
+};
+
+export default config;
