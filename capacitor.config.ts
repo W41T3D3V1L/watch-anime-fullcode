a0505297b89a7cd8925468839af186c2979fdc25
Gollavilli Dhanush Kumar <112778628+Dhanush2468@users.noreply.github.com>
2024-07-10 19:12:37 +0000

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gojo.app',
+  appName: 'gojo.in',
+  webDir: 'build'
+};
+
+export default config;
